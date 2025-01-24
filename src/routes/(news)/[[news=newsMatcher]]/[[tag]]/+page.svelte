3b8af5d9dc28db64bcb5ab6780a1a0c330c4d389
slidenerd <slidenerd@gmail.com>
2025-01-24 19:01:54 +0530

    Create +page.svelte

---
+++ src/routes/(news)/[[news=newsMatcher]]/[[tag]]/+page.svelte
@@ -0,0 +1,17 @@
+<main>
+	<h1>Popular News</h1>
+	<div>This page shows up only under 2 conditions</div>
+	<ol>
+		<li>You did not select any news item yet (you are on the list view)</li>
+		<li>You are on desktop</li>
+	</ol>
+	<div>
+		On mobile, this is accessible under a separate /popular url which doesn't work at the moment
+	</div>
+</main>
+
+<style>
+	main {
+		padding: 1rem;
+	}
+</style>
